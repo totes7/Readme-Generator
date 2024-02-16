0114e8800a3957a303319262b39129a999ad199f
Antonio Manno <antonio.manno.7@gmail.com>
2024-02-16 16:51:13 +0000

    fix the license question to use the licenses array as choices

--- index.js
+++ index.js
@@ -7,67 +7,57 @@ const generateMarkdown = require("./utils/generateMarkdown");
 const licenses = [
   {
     name: "Apache License 2.0",
-    icon: "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)",
+    value: "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)",
   },
   {
     name: "GNU General Public License v3.0",
-    icon: "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)",
+    value: "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)",
   },
   {
     name: "MIT License",
-    icon: "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)",
+    value: "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)",
   },
   {
     name: 'BSD 2-Clause "Simplified" License',
-    icon: "[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)",
+    value: "[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)",
   },
   {
     name: 'BSD 3-Clause "New" or "Revised" License',
-    icon: "[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)",
+    value: "[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)",
   },
   {
     name: "Boost Software License 1.0",
-    icon: "[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)",
+    value: "[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)",
   },
   {
     name: "Creative Commons Zero v1.0 Universal",
-    icon: "[![License: CC0-1.0](https://img.shields.io/badge/License-CC0_1.0-lightgrey.svg)](http://creativecommons.org/publicdomain/zero/1.0/)",
+    value: "[![License: CC0-1.0](https://img.shields.io/badge/License-CC0_1.0-lightgrey.svg)](http://creativecommons.org/publicdomain/zero/1.0/)",
   },
   {
     name: "Eclipse Public License 1.0",
-    icon: "[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)",
+    value: "[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)",
   },
   {
     name: "GNU Affero General Public License v3.0",
-    icon: "[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)",
+    value: "[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)",
   },
   {
     name: "GNU General Public License v2.0",
-    icon: "[![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)",
+    value: "[![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)",
   },
   {
     name: "GNU Lesser General Public License v3.0",
-    icon: "[![License: LGPL v3](https://img.shields.io/badge/License-LGPL_v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)",
+    value: "[![License: LGPL v3](https://img.shields.io/badge/License-LGPL_v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)",
   },
   {
     name: "Mozilla Public License 2.0",
-    icon: "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)",
+    value: "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)",
   },
   {
     name: "The Unlicense",
-    icon: "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)",
+    value: "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)",
   },
 ];
-
-// create array of licenses names from the licenses array to use in the inquirer licenses list
-const licenseArr = Object.entries(licenses);
-
-const licenseNames = [];
-
-for (let i = 0; i < licenseArr.length; i++) {
-    let name = licenseArr[i][1].name;
-    licenseNames.push(name);
-}
 
 // array of questions for user
 const questions = [
@@ -95,7 +85,7 @@ const questions = [
   {
     type: "list",
     message: "Choose your license from the list below.",
-    choices: [licenseNames],
+    choices: licenses,
     name: "license",
   },
 ];
