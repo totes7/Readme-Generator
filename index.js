8932e90236118e149e0f81db5c4e60e961af93c5
Antonio Manno <antonio.manno.7@gmail.com>
2024-02-16 21:18:56 +0000

    create separate file for licenses and import it into index.js

--- index.js
+++ index.js
@@ -2,62 +2,7 @@ const fs = require("fs");
 const path = require("path");
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
-
-// create array of licenses each with a name and the markup code for the icon
-const licenses = [
-  {
-    name: "Apache License 2.0",
-    value: "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)",
-  },
-  {
-    name: "GNU General Public License v3.0",
-    value: "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)",
-  },
-  {
-    name: "MIT License",
-    value: "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)",
-  },
-  {
-    name: 'BSD 2-Clause "Simplified" License',
-    value: "[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)",
-  },
-  {
-    name: 'BSD 3-Clause "New" or "Revised" License',
-    value: "[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)",
-  },
-  {
-    name: "Boost Software License 1.0",
-    value: "[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)",
-  },
-  {
-    name: "Creative Commons Zero v1.0 Universal",
-    value: "[![License: CC0-1.0](https://img.shields.io/badge/License-CC0_1.0-lightgrey.svg)](http://creativecommons.org/publicdomain/zero/1.0/)",
-  },
-  {
-    name: "Eclipse Public License 1.0",
-    value: "[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)",
-  },
-  {
-    name: "GNU Affero General Public License v3.0",
-    value: "[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)",
-  },
-  {
-    name: "GNU General Public License v2.0",
-    value: "[![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)",
-  },
-  {
-    name: "GNU Lesser General Public License v3.0",
-    value: "[![License: LGPL v3](https://img.shields.io/badge/License-LGPL_v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)",
-  },
-  {
-    name: "Mozilla Public License 2.0",
-    value: "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)",
-  },
-  {
-    name: "The Unlicense",
-    value: "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)",
-  },
-];
+const licenses = require('./utils/licences')
 
 // array of questions for user
 const questions = [
@@ -108,7 +53,14 @@ const questions = [
 inquirer
   .prompt(questions)
   .then(function(response) {
-    console.log(response);
+    console.log(response.license);
+    let licenseName = '';
+    for (const {name, value} of licenses) {
+        if (value === response.license) {
+            licenseName += name;
+        }
+    }
+    console.log(licenseName);
   })
 
 // function to write README file
